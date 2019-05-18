91dfb56f3d212fff8f65535e37f35301862935e5
Marek Dudek <marek.dudek@gmail.com>
2019-05-19 00:43:14 +0200

    Spring Boot test with JUnit 5

--- src/test/java/com/marekdudek/springcert/JUnit5Test.java
+++ src/test/java/com/marekdudek/springcert/JUnit5Test.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-final class Sprint5Test
+final class JUnit5Test
 {
     @Test
     void test()
     {
         assertThat(2 + 2).isEqualTo(4);
+        assertThat("Hello," + " World!").hasSize(13);
     }
 }
