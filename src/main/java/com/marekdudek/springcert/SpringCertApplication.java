4b9af9dc9e4a54d32f6d1bf50e1b896f0b3210b3
Marek Dudek <marek.dudek@gmail.com>
2019-05-18 23:26:25 +0200

    reformatted

--- src/main/java/com/marekdudek/springcert/SpringCertApplication.java
+++ src/main/java/com/marekdudek/springcert/SpringCertApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCertApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCertApplication.class, args);
-	}
-
+public class SpringCertApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringCertApplication.class, args);
+    }
 }
--- src/test/java/com/marekdudek/springcert/SpringCertApplicationTests.java
+++ src/test/java/com/marekdudek/springcert/SpringCertApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringCertApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+public class SpringCertApplicationTests
+{
+    @Test
+    public void contextLoads()
+    {
+    }
 }
