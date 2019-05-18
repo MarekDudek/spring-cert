43e7718dc96a23df07777d0ec04b801735528850
Marek Dudek <marek.dudek@gmail.com>
2019-05-18 21:20:21 +0200

    Fresh Spring Boot project

---
+++ src/main/java/com/marekdudek/springcert/SpringCertApplication.java
@@ -0,0 +1,13 @@
+package com.marekdudek.springcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marekdudek/springcert/SpringCertApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marekdudek.springcert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCertApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
