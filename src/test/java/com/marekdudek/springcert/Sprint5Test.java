db730c992f281c5b85d2c7e168bea66387506825
Marek Dudek <marek.dudek@gmail.com>
2019-05-19 00:10:13 +0200

    JUnit 5 configured

---
+++ src/test/java/com/marekdudek/springcert/Sprint5Test.java
@@ -0,0 +1,14 @@
+package com.marekdudek.springcert;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+final class Sprint5Test
+{
+    @Test
+    void test()
+    {
+        assertThat(2 + 2).isEqualTo(4);
+    }
+}
