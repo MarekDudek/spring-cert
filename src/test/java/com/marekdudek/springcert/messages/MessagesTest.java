3b4767c1e791400920973304835bc6e3e64f5532
Marek Dudek <marek.dudek@gmail.com>
2019-05-19 22:40:45 +0200

    Mockito - order of execution

--- src/test/java/com/marekdudek/springcert/messages/MessagesTest.java
+++ src/test/java/com/marekdudek/springcert/messages/MessagesTest.java
@@ -4,14 +4,15 @@ import com.marekdudek.springcert.messages.implementations.RunCountOfTimesAction;
 import com.marekdudek.springcert.messages.interfaces.MessageAction;
 import com.marekdudek.springcert.messages.interfaces.MessageConsumer;
 import com.marekdudek.springcert.messages.interfaces.MessageSupplier;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 final class MessagesTest
@@ -21,18 +22,43 @@ final class MessagesTest
     @Mock
     private MessageConsumer consumer;
 
+    // System under test
+    private MessageAction action;
+
+    @BeforeEach
+    void setUp()
+    {
+        given(supplier.message()).willReturn(MESSAGE);
+        action = new RunCountOfTimesAction(supplier, consumer, TIMES);
+    }
+
     private static final String MESSAGE = "message";
     private static final int TIMES = 3;
 
     @Test
     void test()
     {
-        given(supplier.message()).willReturn(MESSAGE);
         // when
-        final MessageAction action = new RunCountOfTimesAction(supplier, consumer, TIMES);
         action.run();
         // then
         verify(supplier, times(TIMES)).message();
         verify(consumer, times(TIMES)).render(MESSAGE);
+        verifyNoMoreInteractions(supplier, consumer);
+    }
+
+    @Test
+    void order_of_execution()
+    {
+        // when
+        action.run();
+        // then
+        final InOrder order = inOrder(supplier, consumer);
+        order.verify(supplier).message();
+        order.verify(consumer).render(MESSAGE);
+        order.verify(supplier).message();
+        order.verify(consumer).render(MESSAGE);
+        order.verify(supplier).message();
+        order.verify(consumer).render(MESSAGE);
+        verifyNoMoreInteractions(supplier, consumer);
     }
 }
