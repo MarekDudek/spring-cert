54c6fca45f99bd778e84563f236504b9ffa50f43
Marek Dudek <marek.dudek@gmail.com>
2019-05-19 13:56:26 +0200

    spring boot tests of messages pipeline

---
+++ src/main/java/com/marekdudek/springcert/messages/MessagesConfig.java
@@ -0,0 +1,32 @@
+package com.marekdudek.springcert.messages;
+
+import com.marekdudek.springcert.messages.implementations.ConstMessageSupplier;
+import com.marekdudek.springcert.messages.implementations.PrintStreamMessageConsumer;
+import com.marekdudek.springcert.messages.implementations.RunCountOfTimesPipeline;
+import com.marekdudek.springcert.messages.interfaces.MessageConsumer;
+import com.marekdudek.springcert.messages.interfaces.MessagePipeline;
+import com.marekdudek.springcert.messages.interfaces.MessageSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class MessagesConfig
+{
+    @Bean
+    MessageSupplier supplier()
+    {
+        return new ConstMessageSupplier("some message");
+    }
+
+    @Bean
+    MessageConsumer consumer()
+    {
+        return new PrintStreamMessageConsumer(System.out);
+    }
+
+    @Bean
+    MessagePipeline pipeline()
+    {
+        return new RunCountOfTimesPipeline(3);
+    }
+}
---
+++ src/test/java/com/marekdudek/springcert/messages/MessagesSpringBootTest.java
@@ -0,0 +1,25 @@
+package com.marekdudek.springcert.messages;
+
+import com.marekdudek.springcert.messages.interfaces.MessageConsumer;
+import com.marekdudek.springcert.messages.interfaces.MessagePipeline;
+import com.marekdudek.springcert.messages.interfaces.MessageSupplier;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+final class MessagesSpringBootTest
+{
+    @Autowired
+    private MessageSupplier supplier;
+    @Autowired
+    private MessageConsumer consumer;
+    @Autowired
+    private MessagePipeline pipeline;
+
+    @Test
+    void test()
+    {
+        pipeline.run(supplier, consumer);
+    }
+}
