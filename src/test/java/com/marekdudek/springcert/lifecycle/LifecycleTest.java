e00a0f541d3d85094a4fb5db5524572b8a0ba99a
Marek Dudek <marek.dudek@gmail.com>
2019-05-26 19:03:53 +0200

    more checks in lifecycle test

--- src/test/java/com/marekdudek/springcert/lifecycle/LifecycleTest.java
+++ src/test/java/com/marekdudek/springcert/lifecycle/LifecycleTest.java
@@ -27,23 +27,42 @@ class LifecycleTest
         // when
         beanFactory.destroySingletons();
         // then
-        assertThat(cycling.preDestroyed).isTrue();
-        assertThat(cycling.destroyed).isTrue();
-        assertThat(cycling.stopped).isTrue();
+        assertIsFinalized(cycling);
         // FIXME: this can break when tests are executed in different order
+        // when
+        final Cycling bean = beanFactory.getBean(Cycling.class);
+        // then
+        bean.wheel.setCycling(bean);
+        assertIsInitialized(bean);
+        assertIsNotFinalized(bean);
     }
 
     @Test
     void during_test_bean_is_initialized_and_not_finalized()
     {
-        assertThat(cycling.name).isEqualTo("cycling");
+        // then
+        assertIsInitialized(cycling);
+        assertIsNotFinalized(cycling);
+    }
 
+    private static void assertIsInitialized(final Cycling cycling)
+    {
         assertThat(cycling.constructed).isTrue();
         assertThat(cycling.propertiesSet).isTrue();
         assertThat(cycling.started).isTrue();
+    }
 
+    private static void assertIsNotFinalized(final Cycling cycling)
+    {
         assertThat(cycling.preDestroyed).isFalse();
         assertThat(cycling.destroyed).isFalse();
         assertThat(cycling.stopped).isFalse();
+    }
+
+    private static void assertIsFinalized(final Cycling cycling)
+    {
+        assertThat(cycling.preDestroyed).isTrue();
+        assertThat(cycling.destroyed).isTrue();
+        assertThat(cycling.stopped).isTrue();
     }
 }
