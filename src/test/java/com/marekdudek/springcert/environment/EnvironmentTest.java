5b9d46442fe736dbd8ab5cf0c9fd2a5e3c5e8fe0
Marek Dudek <marek.dudek@gmail.com>
2019-06-01 18:28:48 +0200

    custom property source

--- src/test/java/com/marekdudek/springcert/environment/EnvironmentTest.java
+++ src/test/java/com/marekdudek/springcert/environment/EnvironmentTest.java
@@ -1,11 +1,13 @@
 package com.marekdudek.springcert.environment;
 
+import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
+import org.springframework.core.env.*;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -16,6 +18,9 @@ final class EnvironmentTest
 {
     @Autowired
     private Environment environment;
+    @Autowired
+    private ConfigurableEnvironment configurableEnvironment;
+
 
     @Test
     void active_profiles()
@@ -45,14 +50,36 @@ final class EnvironmentTest
     }
 
     @Test
-    void properties()
+    void reading_properties()
     {
         assertThatCode(() ->
                 {
                     environment.getRequiredProperty("user.name");
-                    environment.getRequiredProperty("os.name");
+                    environment.getRequiredProperty("HOME");
                     environment.getRequiredProperty("spring.profiles.active");
                 }
         ).doesNotThrowAnyException();
+    }
+
+    @Test
+    void resolving_placeholders()
+    {
+        assertThatCode(() ->
+                environment.resolveRequiredPlaceholders("This is ${spring.application.name} application.")
+        ).doesNotThrowAnyException();
+    }
+
+    @Test
+    void adding_custom_property_source()
+    {
+        // given
+        final ImmutableMap<String, Object> map = ImmutableMap.of("one", 1, "two", 2);
+        // when
+        final PropertySource<Map<String, Object>> source = new MapPropertySource("map", map);
+        final MutablePropertySources sources = configurableEnvironment.getPropertySources();
+        sources.addFirst(source);
+        // then
+        final Integer property = configurableEnvironment.getRequiredProperty("one", Integer.class);
+        assertThat(property).isEqualTo(1);
     }
 }
